2a62204b7eed7e18edcd3e05419be7a162e1f35c
Eliel WOTOBE <elielwotobe@gmail.com>
2026-07-21 13:05:22 +0200

    feat: implement complete quiz system with submission logic, history tracking, and full-stack API integration

--- frontend/src/api/quiz.api.js
+++ frontend/src/api/quiz.api.js
@@ -4,3 +4,20 @@ export async function soumettreTentative(quizId, reponses) {
   const response = await axiosClient.post(`/quiz/${quizId}/tentative`, { reponses });
   return response.data; // { score_obtenu, est_reussi, nb_bonnes_reponses, total_questions, score_min_requis }
 }
+
+export async function createQuiz(data){
+  const response = await axiosClient.post('/quiz', data);
+  return response.data;
+}
+
+export async function getQuizById(quizId) {
+  const response = await axiosClient.get(`/quiz/${quizId}`);
+  return response.data;
+}
+
+export async function getTentatives(quizId) {
+  const response = await axiosClient.get(`/quiz/${quizId}/tentatives`);
+  return response.data;
+}
+
+
